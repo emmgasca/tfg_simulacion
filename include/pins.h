5e9707c1befe17a968b19974ac3332d1de74cddc
Emma Gascón <817993@unizar.es>
2026-06-04 17:05:48 +0200

    Convertir archivos txt a cpp

--- include/pins.h
+++ include/pins.h
@@ -5,6 +5,3 @@ constexpr uint8_t LED_PIN_RGB_Green = GPIO_NUM_5;
 constexpr uint8_t LED_PIN_RGB_Red   = GPIO_NUM_6;
 constexpr uint8_t LED_PIN_RGB_Blue  = GPIO_NUM_7;
 constexpr uint8_t LED_PIN_Orange    = GPIO_NUM_1;
-// ==========================================
-// SELECCIÓN DE HARDWARE
-// ==========================================
