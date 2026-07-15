c5f5b38b0acf9b211019338cb086ba5523b09756
emmgasca <817993@unizar.es>
2026-07-15 20:46:46 +0200

    Paso 3: Recibir los datos EMG en crudo

---
+++ pc/recepcion.py
@@ -0,0 +1,17 @@
+import asyncio
+from bleak import BleakClient
+
+CARACTERISTICA_EMG = "AAAAAAAA-1234-1234-1234-123456789ABC"
+DIRECCION = "E8:3D:C1:F6:09:09"
+
+def cuando_llega_dato(caracteristica, datos):
+    print(len(datos), datos.hex())
+    
+async def main():
+    async with BleakClient(DIRECCION) as client:
+        print(f"Conectado al dispositivo BLE: {client.is_connected}")
+        await client.start_notify(CARACTERISTICA_EMG, cuando_llega_dato)
+        await asyncio.sleep(5)
+
+asyncio.run(main())
+
