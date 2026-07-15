8d11f6390621b2e2b14599e2ffcc6735bdd21d36
emmgasca <817993@unizar.es>
2026-07-15 20:51:22 +0200

    Paso 4: Decodificar los 4 bytes a un float

--- pc/recepcion.py
+++ pc/recepcion.py
@@ -1,11 +1,13 @@
 import asyncio
 from bleak import BleakClient
+import struct
 
 CARACTERISTICA_EMG = "AAAAAAAA-1234-1234-1234-123456789ABC"
 DIRECCION = "E8:3D:C1:F6:09:09"
 
 def cuando_llega_dato(caracteristica, datos):
-    print(len(datos), datos.hex())
+    valor = struct.unpack('<f', datos)[0]
+    print(valor)
     
 async def main():
     async with BleakClient(DIRECCION) as client:
